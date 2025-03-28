679f4574170387868a304aa55dfdfde47cf57d29
Alex <lisatthu35@gmail.com>
2025-03-28 16:56:03 +0700

    Update MainController.java

--- src/main/java/restcontroller/MainController.java
+++ src/main/java/restcontroller/MainController.java
@@ -60,8 +60,8 @@ public class MainController {
     String startAuto() {
         try {
             if (VariableSession.flag_status_is_first_run_app) {
-                Runtime.getRuntime().exec("google-chrome-stable --headless --no-sandbox --disable-dev-shm-usage --disable-gpu --remote-debugging-port=9222 &");
-                Thread.sleep(4000);
+                //Runtime.getRuntime().exec("google-chrome-stable --headless --no-sandbox --disable-dev-shm-usage --disable-gpu --remote-debugging-port=9222 &");
+                //Thread.sleep(4000);
                 webDriver = createWebdriver.getGoogle(Constant.binaryGoogleHeroku);
                 VariableSession.flag_status_is_first_run_app = false;
                 myAction1 = new Actions(MainController.webDriver);
